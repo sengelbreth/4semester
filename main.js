d6e72109c1a19a4b2b90cceddce6692e80f6fadb
sengelbreth <sophie.engel1202@gmail.com>
2019-05-06 11:12:26 +0200

    undersider menu

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", HentGentagelser);
 async function HentGentagelser() {
   let headerData = await fetch("header.html");
   let header = await headerData.text();
-  document.querySelector("#hovedmenu").innerHTML = header;
+  document.querySelector(".hovedmenu").innerHTML = header;
 
   let footerData = await fetch("footer.html");
   let footer = await footerData.text();
