606c8c19d3f4e4aac3d91344c81a23090fe8b0b3
sophie <sophie.engel1202@gmail.com>
2019-06-03 12:02:45 +0200

    links i footeren

--- kontakt.js
+++ kontakt.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", modalLisners);
 function modalLisners() {
   document.querySelector("#submitbtn").addEventListener("click", openModal);
   document.querySelector("#myModal").addEventListener("click", lukModalVindue);
+  document.querySelector(".close").addEventListener("click", closeModal);
 }
 
-document.querySelector(".close").addEventListener("click", closeModal);
 let modal = document.querySelector("#myModal");
 function openModal() {
   modal.style.display = "block";
